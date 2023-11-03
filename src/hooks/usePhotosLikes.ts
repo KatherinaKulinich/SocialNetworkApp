5eb9f1792de5d619c8dfb65c7b11b2f46615ca66
KatherinaKulinich <kulinichcatherina@gmail.com>
2023-11-03 16:13:27 +0200

    updated feature for toggling photos likes

--- src/hooks/usePhotosLikes.ts
+++ src/hooks/usePhotosLikes.ts
@@ -1,131 +1,92 @@
-import { useCallback, useEffect, useState } from "react"
-import { useAuth } from "./useAuth"
+import { useCallback } from "react"
 import { useUserData } from "./useUserData"
-import { message } from "antd"
-import { db, storage } from "firebase"
-import { addDoc, collection, doc, onSnapshot, setDoc, updateDoc } from "firebase/firestore"
-import { ref, uploadBytesResumable, getDownloadURL, deleteObject } from "firebase/storage"
+import { db } from "firebase"
+import { DocumentData, DocumentReference, doc, updateDoc } from "firebase/firestore"
 import { Photo } from "types/Photo"
-import {v4 as uuidv4} from 'uuid'
-import { useAppDispatch, useAppSelector } from "./hooks"
-import { fetchUserFullData } from "rdx/slices/userDataSlice"
 import { UserFullData } from "types/UserFullDataType"
-import { Post } from "types/Post"
+
 
 
 
 export const usePhotosLikes = () => {
-
     const { userData } = useUserData()
-    const { userId, userName, photos } = userData;
+    const { userId:myId, userName, photos:myPhotos } = userData;
+    const myRef = doc(db, "users", myId);
     
-    const userRef = doc(db, "users", userId);
-    const dispatch = useAppDispatch()
-
-    const selectedPhoto = useAppSelector(state => state.content.selectedPhoto)
-    const {photoLikes} = selectedPhoto;
-
-
 
 
     const checkUserLikeReaction = useCallback((photo: Photo) => {
-        if (photo?.photoLikes?.includes(userId)) {
+        if (photo?.photoLikes?.includes(myId)) {
             return true
         }
         return false
     }, [userData])
-        
 
 
-    const addLike = useCallback((userId:string, photo:Photo) => {
-        const newLikesArray = [...photo.photoLikes, userId]
-        console.log(newLikesArray);
-        
-
-        const updatedPhoto:Photo = {...photo, photoLikes: newLikesArray};
+    
+    const updatePhotos = useCallback((photos:Photo[], updatedPhoto:Photo, ref:DocumentReference<DocumentData, DocumentData>) => {
         const updatedPhotoArray = photos?.map((photo:Photo) => {
             if (photo.photoId === updatedPhoto.photoId) {
                 return updatedPhoto
             }
             return photo
         })
-        console.log(updatedPhotoArray);
-
-        updateDoc(userRef, {
+        updateDoc(myRef, {
             photos: updatedPhotoArray,
         })
+    }, [])
+        
+
+
+
+    const addLike = useCallback((photo:Photo, user:UserFullData) => {
+        const { userId, photos:userPhotos } = user;
+        const userRef = doc(db, "users", userId);
+
+        const newLikesArray = [...photo.photoLikes, myId]
+        const updatedPhoto:Photo = {...photo, photoLikes: newLikesArray};
+
+        if (myId === userId) {
+            updatePhotos(myPhotos as Photo[], updatedPhoto, myRef)
+            return
+        }
+        updatePhotos(userPhotos as Photo[], updatedPhoto, userRef)
     }, [userData])
 
 
 
-    const removeLike = useCallback((userId:string, photo:Photo) => {
+    const removeLike = useCallback((photo:Photo, user:UserFullData) => {
+        const { userId, photos:userPhotos } = user;
+        const userRef = doc(db, "users", userId);
+
         const newLikesArray = photo?.photoLikes.filter(like => like !== userId)
         const updatedPhoto:Photo = {...photo, photoLikes: newLikesArray};
 
-        const updatedPhotoArray = photos?.map((photo:Photo) => {
-            if (photo.photoId === updatedPhoto.photoId) {
-                return updatedPhoto
-            }
-            return photo
-        })
-
-        updateDoc(userRef, {
-            photos: updatedPhotoArray,
-        })
-
+        if (myId === userId) {
+            updatePhotos(myPhotos as Photo[], updatedPhoto, myRef)
+            return
+        }
+        updatePhotos(userPhotos as Photo[], updatedPhoto, userRef)
     }, [userData])
 
 
 
-    const onToggleLike = useCallback((userId:string, photo:Photo) => {
-        const isLike = checkUserLikeReaction(userId, photo)
-        console.log('isLike', checkUserLikeReaction(userId, photo) );
+    const onToggleLike = useCallback((photo:Photo, user:UserFullData) => {
+        const isLike = checkUserLikeReaction(photo)
+        // console.log('isLike', checkUserLikeReaction(photo) );
         
-
         if (isLike === true) {
-            removeLike(userId, photo)
+            removeLike(photo, user)
         } else if (isLike === false) {
-            addLike(userId, photo)
+            addLike(photo, user)
         }
 
     }, [userData])
 
 
-
-    // const sendNewComment = useCallback((newCommentText:string, photo:Photo, user:UserFullData) => {
-    //     const { userId, fullname, userAvatar } = user;
-    //     const commentsArray = photo.photoComments
-    //     const newCommentsArray = [...commentsArray, {
-    //         userId,
-    //         userName: fullname,
-    //         userAvatar,
-    //         text: newCommentText,
-    //         date: Date.now(),
-    //     }]
-
-    //     const updatedPhoto:Photo = {...photo, photoComments: newCommentsArray};
-    //     const updatedPhotoArray = photos?.map((photo:Photo) => {
-    //         if (photo.photoId === updatedPhoto.photoId) {
-    //             return updatedPhoto
-    //         }
-    //         return photo
-    //     })
-
-    //     updateDoc(userRef, {
-    //         photos: updatedPhotoArray,
-    //     })
-
-    // }, [userData, photos])
-
-
     
     return {
-        // addNewPhoto,
-        // selectedPhoto,
-        // deleteUserPhoto,
-        // editUserPhoto,
         checkUserLikeReaction,
         onToggleLike,
-        // sendNewComment
     }
 }
